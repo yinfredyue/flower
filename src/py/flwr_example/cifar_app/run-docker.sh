75209b5d81dd73da8ae7de53100ae670e4b2d807
Fred Yue Yin <yy0125@connect.hku.hk>
2020-11-02 22:40:16 +0800

    update

--- src/py/flwr_example/cifar_app/run-docker.sh
+++ src/py/flwr_example/cifar_app/run-docker.sh
@@ -2,10 +2,10 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../../
 
-docker run -d --rm --network flower --name logserver pytorchcpu:latest \
+docker run -d --rm --network flower --name logserver flowerpytorch:latest \
   python3 -m flwr_experimental.logserver
 
-docker run -d --rm --network flower --name server pytorchcpu:latest \
+docker run -d --rm --network flower --name server flowerpytorch:latest \
   python3 -m flwr_example.cifar_app.server \
   --rounds=1 \
   --sample_fraction=1.0 \
@@ -16,10 +16,10 @@ docker run -d --rm --network flower --name server pytorchcpu:latest \
 # When running without docker, --server_address=[::]:8080 to indicate local machine.
 # When running with docker, --server_address=server:8080, where server is the name of 
 # container inside the docker network.
-docker run -d --rm --network flower --name client_0 pytorchcpu:latest \
+docker run -d --rm --network flower --name client_0 flowerpytorch:latest \
   python3 -m flwr_example.cifar_app.client --cid=0 --server_address=server:8080 --log_host=logserver:8081
 
-docker run -d --rm --network flower --name client_1 pytorchcpu:latest \
+docker run -d --rm --network flower --name client_1 flowerpytorch:latest \
   python3 -m flwr_example.cifar_app.client --cid=1 --server_address=server:8080 --log_host=logserver:8081
 
 docker exec logserver tail -f flower_logs/flower.log
